680cd74ffae58d9e63b890df81847e7ae7c9e1c0
Lim, Sheng Ting (Student) <sl774@students.waikato.ac.nz>
2025-05-12 21:30:38 +0000

    Client: Get and parse hostname, port and filename

---
+++ Client.java
@@ -0,0 +1,19 @@
+public class Client {
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Usage: java Client <hostname> <port> <filename>");
+            return;
+        }
+
+        String hostname = args[0];
+        int port;
+        String filename = args[2];
+
+        try {
+            port = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid port number. Please provide a valid integer.");
+            return;
+        }
+    }
+}
