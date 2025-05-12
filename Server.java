b851e32e3e45017dd236f86f398044905226c29f
Lim, Sheng Ting (Student) <sl774@students.waikato.ac.nz>
2025-05-12 21:27:03 +0000

    Server: Initialise DatagramSocket to port

--- Server.java
+++ Server.java
@@ -1,3 +1,5 @@
+import java.net.DatagramSocket;
+
 public class Server {
     public static void main(String[] args) {
         if (args.length != 1) {
@@ -12,6 +14,13 @@ public class Server {
         } catch (NumberFormatException e) {
             System.out.println("Invalid port number. Please provide a valid integer.");
             return;
+        }
+
+        try (DatagramSocket serverSocket = new DatagramSocket(port)) {
+            
+        }
+        catch (Exception e){
+            
         }
     }
 }
