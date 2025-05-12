44e312bdacfc1d18736f990aa0f7ad242440f539
Sheng Ting Lim <sl774@students.waikato.ac.nz>
2025-05-13 09:24:48 +1200

    Server: Get and parse port number

--- server.java
+++ server.java
@@ -1,3 +1,17 @@
-public class server {
+public class Server {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java Server <port>");
+            return;
+        }
 
+        int port;
+
+        try {
+            port = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid port number. Please provide a valid integer.");
+            return;
+        }
+    }
 }
