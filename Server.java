4d7d6e72efc628f167ce41ef98cd9ef8cee796c7
Sheng Ting Lim <sl774@students.waikato.ac.nz>
2025-05-16 22:12:50 +1200

    Server: Implement basic ClientHandler thread class

--- Server.java
+++ Server.java
@@ -33,12 +33,43 @@ public class Server {
                 InetAddress clientAddress = requestPacket.getAddress();
                 int clientPort = requestPacket.getPort();
                 System.out.println("Client address: " + clientAddress + ", Client port: " + clientPort);
-
                 
+
             }
         }
         catch (Exception e){
             
+        }
+    }
+
+    static class ClientHandler extends Thread {
+        private String fileName;
+        private String filePath;
+        private InetAddress clientAddress;
+        private int clientHandlerPort;
+
+        public ClientHandler(String fileName, String filePath, InetAddress clientAddress, int clientHandlerPort) {
+            this.fileName = fileName;
+            this.filePath = filePath;
+            this.clientAddress = clientAddress;
+            this.clientHandlerPort = clientHandlerPort;
+            System.out.println("ClientHandler created for file: " + fileName + " on port: " + clientHandlerPort);
+        }
+
+        @Override
+        public void run() {
+            try (DatagramSocket socket = new DatagramSocket(clientHandlerPort)) {
+                System.out.println("ClientHandler started for file: " + fileName + " on port: " + clientHandlerPort);
+                byte[] buffer = new byte[1024];
+                
+                while (true) {
+                    DatagramPacket filePacket = new DatagramPacket(buffer, buffer.length);
+                    socket.receive(filePacket);
+                    System.out.println("File data received from client: " + fileName);
+                }
+            } catch (Exception e) {
+                System.out.println("Error sending file data: " + e.getMessage());
+            }
         }
     }
 }
